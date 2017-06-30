ae13b8e895bfe16f5af21bc4f8fe30265a1a3099
Shoosty <shoosty1@mac.com>
2017-06-30 10:11:59 -0400

    refactor Faker file

--- db/faker.rb
+++ db/faker.rb
@@ -1,16 +1,21 @@
 require File.expand_path('../../config/environment', __FILE__)
 
 # add reocrds to the sandbox deveolpment database
-class AddRecs
+class AddFakeRecords
   def add_fake_records
-    a = 0
+    counter = 0
 
-    until a == 100
-      Sandbox.create(fname: Faker::Name.first_name,lname: Faker::Name.last_name,
-      user: Faker::Artist.name, email: Faker::Internet.email, password: Faker::Crypto.md5)
-      a += 1
+    until counter == 100
+      Sandbox.create(
+        fname: Faker::Name.first_name,
+        lname: Faker::Name.last_name,
+        user: Faker::Artist.name,
+        email: Faker::Internet.email,
+        password: Faker::Crypto.md5
+        )
+      counter += 1
     end
   end
 end
 
-AddRecs.new.add_fake_records
+AddFakeRecords.new.add_fake_records
