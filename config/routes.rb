c1008b9cddec6ace085331ed586dd7f8aa977c33
Shoosty <shoosty1@mac.com>
2017-07-01 11:46:43 -0400

    user-show and user-delete

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#index'
-  get 'about' => 'static_pages#about'
+
+  get 'about'   => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
-  get 'demos' => 'static_pages#demos'
-  get 'notes' => 'static_pages#notes'
-  get 'login' => 'sessions#login'
+  get 'demos'   => 'static_pages#demos'
+  get 'notes'   => 'static_pages#notes'
+
+  get 'login'   => 'sessions#login'
+
+  resource :sandbox
+  #rake routes to see the magic
 
 end
