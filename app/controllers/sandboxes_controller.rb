bd81cd39faa7bfce82dd82f8534376c899ef309a
Shoosty <shoosty1@mac.com>
2017-07-02 09:42:43 -0400

    add and delete records with active record

--- app/controllers/sandboxes_controller.rb
+++ app/controllers/sandboxes_controller.rb
@@ -1,2 +1,10 @@
 class SandboxesController < ApplicationController
+  def sandbox_params
+    params.require(:sandbox).permit!
+  end
+
+  def create
+    sandbox_params
+    Sandbox.create(sandbox_params)
+  end
 end
