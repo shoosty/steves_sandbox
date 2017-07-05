ade92e27eff18a78ace58b2884708b1f6988faae
Shoosty <shoosty1@mac.com>
2017-07-04 23:31:59 -0400

    user destroy

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,40 @@
 class UsersController < ApplicationController
 # controller methods never take parameters!
-  def show
+
+  def user_params
+    params.require(:user).permit!
+  end
+
+  def create
+    @user = User.new(user_params)
+  end
+
+  def user
     @user = User.find_by(id: params[:id])
   end
+
+  def show
+    user
+  end
+
+  def edit
+    user
+  end
+
+  def update
+    user.update(user_params)
+        redirect_to "/users"
+  end
+
+  def destroy
+    user.destroy
+    redirect_to "/users"
+  end
+
+  def new
+    @user = User.new
+  end
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,6 @@ Rails.application.routes.draw do
 
   resource :sandbox
   resources :users
-
   #rake routes to see the magic
 
 end
