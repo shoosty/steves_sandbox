b76174697997ea9550c93c8aa92bda5e1fd07d22
Shoosty <shoosty1@mac.com>
2017-07-06 09:53:32 -0400

    install simple_form and generate model for accounts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170704143403) do
+ActiveRecord::Schema.define(version: 20170706135212) do
+
+  create_table "accounts", force: :cascade do |t|
+    t.text "accountholder"
+    t.text "location"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "sandboxes", force: :cascade do |t|
     t.datetime "created_at", null: false
